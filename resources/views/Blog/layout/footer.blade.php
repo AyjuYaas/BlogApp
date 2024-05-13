e88b80d19129bdce6febc2be426c4a9bc7ba40b6
SAYUJYA SATYAL <sayujya57@gmail.com>
2024-05-13 20:36:56 +0545

    Fix Broken Fontawesome link

--- resources/views/Blog/layout/footer.blade.php
+++ resources/views/Blog/layout/footer.blade.php
@@ -4,6 +4,9 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://kit.fontawesome.com/{{ env('FONT_AWESOME_KIT_KEY') }}.js" crossorigin="anonymous"></script>
+<script src="
+https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.2/js/all.min.js
+"></script>
 </body>
 
 </html>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,6 +6,9 @@
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
 </script>
 <script src="https://kit.fontawesome.com/{{ env('FONT_AWESOME_KIT_KEY') }}.js" crossorigin="anonymous"></script>
+<script src="
+https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.2/js/all.min.js
+"></script>
 </body>
 
 </html>
